d24b8124e472a124bb1ae4efa152ed8232d73d33
Nikolay Kilifarev <nkilifarev@gmail.com>
2025-10-27 08:34:55 -0200

    Devision, Abs Subraction added; Introduced authorization

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -4,8 +4,23 @@ namespace GitExercise
 {
     public class Startup
     {
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain access: "); string password = Console.ReadLine(); Console.Clear();
+            return password == Password;
+        }
+        private const string Password
+        =
+        "abcd1234";
         public static void Main()
         {
+
+            bool isAuthorized = CheckCredentials();
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Access denied."); Console.ReadKey(intercept: true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -35,10 +50,16 @@ namespace GitExercise
                 case "m":
                     OptionsManager.Multiply(a, b);
                     break;
-            }
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a, b);
+                    break;
 
-            Console.WriteLine("Pres any key to close the app...");
-            Console.ReadKey(true);
+                Console.WriteLine("Pres any key to close the app...");
+                Console.ReadKey(true);
+            }
         }
     }
 }
