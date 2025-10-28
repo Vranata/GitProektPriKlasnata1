e2e6bc61f949570fa2cf12df77b91c97cedec748
Stiliyan Petrov <stilian955@gmail.com>
2025-10-28 13:25:43 +0200

    Log, Power, Fact options added

--- GitExercise/OptionsManager.cs
+++ GitExercise/OptionsManager.cs
@@ -40,7 +40,18 @@ namespace GitExercise
 
         internal static void Factorial(double a, double b)
         {
-            throw new NotImplementedException();
+            long factA = CalculateFact((int)a);
+            long factB = CalculateFact((int)b);
+            Console.WriteLine($"{a}! + {b}! = {(int)a}! + {(int)b}! = {factA + factB}");
+        }
+        private static long CalculateFact(int a)
+        {
+            long result = a;
+            for (int i = a - 1; i >= 1; i--)
+            {
+                result = result * i;
+            }
+            return result;
         }
     }
 }
