64beb727e775427a0932f917b03a97b3e4329965
Stiliyan Petrov <stilian955@gmail.com>
2025-10-28 13:11:21 +0200

    Log, Power, Fact options added

--- GitExercise/OptionsManager.cs
+++ GitExercise/OptionsManager.cs
@@ -7,7 +7,10 @@ namespace GitExercise
         public static string[] OptionsList = {
             "a - Add",
             "s - Subtract",
-            "m - Multiply"
+            "m - Multiply",
+            "pow - Power",
+            "log - Logarithm",
+            "fact - sum of 2 factorials"
         };
 
         public static void Add(double a, double b)
@@ -23,6 +26,21 @@ namespace GitExercise
         public static void Subtract(double a, double b)
         {
             Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        internal static void Power(double a, double b)
+        {
+            Console.WriteLine($"{a} ^ {b} = {Math.Pow(a, b)}");
+        }
+
+        internal static void Log(double a, double b)
+        {
+            Console.WriteLine($"Log of {a} with base {b} = {Math.Log(a, b)}");
+        }
+
+        internal static void Factorial(double a, double b)
+        {
+            throw new NotImplementedException();
         }
     }
 }
